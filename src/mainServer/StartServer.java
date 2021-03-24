031630e43d646af670e73cfaed4ac0b080f4c5b1
TheGamerH18 <44879628+TheGamerH18@users.noreply.github.com>
2021-03-24 17:17:10 +0100

    Adding Print with Current Version on start

--- src/mainServer/StartServer.java
+++ src/mainServer/StartServer.java
@@ -3,6 +3,10 @@ package mainServer;
 public class StartServer {
     public static void main(String[] args) {
 
+        String version = "V.0.4.0";
+
+        System.out.println("Starte Ver. "+ version);
+
         LeagueServer server = new LeagueServer();
         try {
             server.startgame(server);
